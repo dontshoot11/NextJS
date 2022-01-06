d0a1d77d448e862a2e6b43765dedeb1728805989
dontshoot11 <megadestroyer95@gmail.com>
2022-01-06 19:31:11 +0300

    added new component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,95 +6,117 @@ import { Button, Rating, Tag } from '..';
 import { devlOfNum, priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
 import Image from 'next/image';
+import { useState } from 'react';
+import { Review } from '../Review/Review';
 
 export const Product = ({
     product,
     className,
     ...props
 }: ProductProps): JSX.Element => {
+    const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
     return (
-        <Card className={styles.product}>
-            <div className={styles.logo}>
-                <Image
-                    src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
-                    alt={product.title}
-                    width={70}
-                    height={70}
-                />
-            </div>
-            <div className={styles.title}>{product.title}</div>
-            <div className={styles.price}>
-                {priceRu(product.price)}
-                {product.oldPrice && (
-                    <Tag className={styles.oldPrice} color="green">
-                        {priceRu(product.price - product.oldPrice)}
-                    </Tag>
-                )}
-            </div>
-            <div className={styles.credit}>
-                {priceRu(product.credit)}/
-                <span className={styles.month}>мес</span>
-            </div>
-            <div className={styles.rating}>
-                <Rating rating={product.reviewAvg ?? product.initialRating} />
-            </div>
-            <div className={styles.tags}>
-                {product.categories.map((c) => (
-                    <Tag className={styles.category} key={c} color="ghost">
-                        {c}
-                    </Tag>
+        <>
+            <Card className={styles.product}>
+                <div className={styles.logo}>
+                    <Image
+                        src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+                        alt={product.title}
+                        width={70}
+                        height={70}
+                    />
+                </div>
+                <div className={styles.title}>{product.title}</div>
+                <div className={styles.price}>
+                    {priceRu(product.price)}
+                    {product.oldPrice && (
+                        <Tag className={styles.oldPrice} color="green">
+                            {priceRu(product.price - product.oldPrice)}
+                        </Tag>
+                    )}
+                </div>
+                <div className={styles.credit}>
+                    {priceRu(product.credit)}/
+                    <span className={styles.month}>мес</span>
+                </div>
+                <div className={styles.rating}>
+                    <Rating
+                        rating={product.reviewAvg ?? product.initialRating}
+                    />
+                </div>
+                <div className={styles.tags}>
+                    {product.categories.map((c) => (
+                        <Tag className={styles.category} key={c} color="ghost">
+                            {c}
+                        </Tag>
+                    ))}
+                </div>
+                <div className={styles.priceTitle}>цена</div>
+                <div className={styles.creditTitle}>кредит</div>
+                <div className={styles.rateTitle}>
+                    {product.reviewCount}
+                    {''}
+                    {devlOfNum(product.reviewCount, [
+                        'отзыв',
+                        'отзыва',
+                        'отзывов',
+                    ])}
+                </div>
+                <Divider className={styles.hr} />
+                <div className={styles.description}>{product.description}</div>
+                <div className={styles.feature}>
+                    {product.characteristics.map((c) => (
+                        <div className={styles.characteristics} key={c.name}>
+                            <span className={styles.characteristicsName}>
+                                {c.name}
+                            </span>
+                            <span className={styles.characteristicsDots}></span>
+                            <span className={styles.characteristicsValue}>
+                                {c.value}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.advBlock}>
+                    {product.advantages && (
+                        <div className={styles.advantages}>
+                            <div className={styles.advTitle}>Преимущества</div>
+                            {product.advantages}
+                        </div>
+                    )}
+                    {product.disadvantages && (
+                        <div className={styles.disadvantages}>
+                            <div className={styles.advTitle}>Недостатки</div>
+                            {product.disadvantages}
+                        </div>
+                    )}
+                </div>
+                <Divider className={styles.hr} />
+                <div className={styles.actions}>
+                    <Button appearance="primary">Узнать подробнее</Button>
+                    {product.reviews && product.reviews.length > 0 && (
+                        <Button
+                            className={styles.reviewButton}
+                            appearance="ghost"
+                            arrow={isReviewOpened ? 'down' : 'right'}
+                            onClick={() => setIsReviewOpened(!isReviewOpened)}
+                        >
+                            Читать отзывы
+                        </Button>
+                    )}
+                </div>
+            </Card>
+            <Card
+                color="blue"
+                className={cn(styles.reviews, {
+                    [styles.opened]: isReviewOpened,
+                    [styles.closed]: !isReviewOpened,
+                })}
+            >
+                {product.reviews.map((r) => (
+                    <Review key={r._id} review={r} />
                 ))}
-            </div>
-            <div className={styles.priceTitle}>цена</div>
-            <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.rateTitle}>
-                {product.reviewCount}
-                {''}
-                {devlOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
-            </div>
-            <Divider className={styles.hr} />
-            <div className={styles.description}>{product.description}</div>
-            <div className={styles.feature}>
-                {product.characteristics.map((c) => (
-                    <div className={styles.characteristics} key={c.name}>
-                        <span className={styles.characteristicsName}>
-                            {c.name}
-                        </span>
-                        <span className={styles.characteristicsDots}></span>
-                        <span className={styles.characteristicsValue}>
-                            {c.value}
-                        </span>
-                    </div>
-                ))}
-            </div>
-            <div className={styles.advBlock}>
-                {product.advantages && (
-                    <div className={styles.advantages}>
-                        <div className={styles.advTitle}>Преимущества</div>
-                        {product.advantages}
-                    </div>
-                )}
-                {product.disadvantages && (
-                    <div className={styles.disadvantages}>
-                        <div className={styles.advTitle}>Недостатки</div>
-                        {product.disadvantages}
-                    </div>
-                )}
-            </div>
-            <Divider className={styles.hr} />
-            <div className={styles.actions}>
-                <Button appearance="primary">Узнать подробнее</Button>
-                <Button
-                    className={styles.reviewButton}
-                    appearance="ghost"
-                    arrow={'right'}
-                >
-                    Читать отзывы
-                </Button>
-            </div>
-        </Card>
+            </Card>
+        </>
     );
 };
-function declOfNum(): import('react').ReactNode {
-    throw new Error('Function not implemented.');
-}
